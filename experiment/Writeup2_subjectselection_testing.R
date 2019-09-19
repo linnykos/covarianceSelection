da7bb8800476a6438778118c60605c047c0d30c7
Kevin Lin <klin1234@gmail.com>
2019-09-19 10:44:26 -0400

    New test upping 0.975 to 0.99

--- experiment/Writeup2_subjectselection_testing.R
+++ experiment/Writeup2_subjectselection_testing.R
@@ -25,11 +25,11 @@ dat_pfc35 <- lapply(dat_pfc35, function(x){
   scale(x, center = T, scale = T)
 })
 
-dat_pfc35_2 <- lapply(dat_pfc35, function(x){
-  svd_res <- svd(x)
-  res <- svd_res$u[,1:k] %*% diag(svd_res$d[1:k]) %*% t(svd_res$v[,1:k])
-  scale(res, center = T, scale = T)
-})
+# dat_pfc35_2 <- lapply(dat_pfc35, function(x){
+#   svd_res <- svd(x)
+#   res <- svd_res$u[,1:k] %*% diag(svd_res$d[1:k]) %*% t(svd_res$v[,1:k])
+#   scale(res, center = T, scale = T)
+# })
 
 ############################
 
@@ -37,9 +37,9 @@ ncores <- 10
 trials <- 200
 
 save(trials, file = paste0(save_filepath, "/test.RData"))
-stepdown_obj <- covarianceSelection::stepdown_path(dat_pfc35_2, trials = trials, cores = ncores, verbose = verbose,
+stepdown_obj <- covarianceSelection::stepdown_path(dat_pfc35, trials = trials, cores = ncores, verbose = verbose,
                                                    iterations = 7, file = paste0(save_filepath, "/step3_subjectselection_tmp2.RData"),
-                                                   prob = 0.975)
+                                                   prob = 0.99)
 save.image(file = paste0(save_filepath, "/step3_subjectselection_tmp.RData"))
 stepdown_res <- lapply(seq(0, 1, length.out = 21), function(alpha){
   covarianceSelection::stepdown_choose(stepdown_obj, alpha = alpha, return_pvalue = T)
