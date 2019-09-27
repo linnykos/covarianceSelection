4640dad6de8b18d09317161675898a93619a18ee
Kevin Lin <klin1234@gmail.com>
2019-09-27 12:07:46 -0400

    Fixed typo

--- experiment/Writeup2_posthoc_analysis2.R
+++ experiment/Writeup2_posthoc_analysis2.R
@@ -3,7 +3,7 @@ load("/raid6/Kevin/covarianceSelection/results/step3_subjectselection_updated2.R
 idx <- covarianceSelection:::tsourakakis_2014_approximate(g)
 dat_list <- lapply(dat_list[idx], scale)
 dat_our <- do.call(rbind, dat_list)
-adj_our <- covarianceSelection::graphicalModel(dat_all, lambda = "lambda.1se", verbose = T) 
+adj_our <- covarianceSelection::graphicalModel(dat_our, lambda = "lambda.1se", verbose = T) 
 
 
 save.image(file = paste0(save_filepath, "/step4_ouranalysis_tmp.RData"))
