21db068ada763945e8d7a36acd9473b5cf561682
Kevin Lin <klin1234@gmail.com>
2019-09-28 22:38:42 -0400

    Fixed typo

--- experiment/experiment.R
+++ experiment/experiment.R
@@ -5,10 +5,11 @@ doMC::registerDoMC(cores = ncores)
 load("/raid6/Kevin/covarianceSelection/results/step3_subjectselection.RData")
 
 combn_mat <- utils::combn(length(dat_list), 2)
-combn_mat <- combn_mat[,sample(1:nrow(combn_mat), 1000)]
-pval_vec <- apply(combn_mat, 2, function(x){
+combn_mat <- combn_mat[,sample(1:ncol(combn_mat), 1000)]
+pval_vec <- sapply(1:ncol(combn_mat), function(i){
+  x <- combn_mat[,i]
   res <- covarianceSelection::cai_test(dat_list[[x[1]]], dat_list[[x[2]]], cores = ncores, prob = 0.99)
-  print(paste0("Indicies ", paste0(x, collapse = "-"), " : value ", round(res, 3)))
+  print(paste0("Indicies ", i, " : value ", round(res, 3)))
   res
 })
 
