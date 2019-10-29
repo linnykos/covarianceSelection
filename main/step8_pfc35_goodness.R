65dd5eb7f01e3086ed337b689c3739cf98371a3d
Kevin Lin <klin1234@gmail.com>
2019-10-28 22:49:48 -0400

    Fixed typo

--- main/step8_pfc35_goodness.R
+++ main/step8_pfc35_goodness.R
@@ -1,12 +1,12 @@
 selected_idx <- grep("PFC\\.[3-5]", names(dat_list))
 
 set.seed(10)
-goodness_pfc35 <- covarianceSelection::goodness_of_fit(dat_all[selected_idx], permutations = 250, trials = 250, prob = 1)
+goodness_pfc35 <- covarianceSelection::goodness_of_fit(dat_list[selected_idx], permutations = 250, trials = 250, prob = 1)
 
 save.image(file = paste0(save_filepath, "/step8_pfc35_goodness", filepath_suffix, ".RData"))
 
 set.seed(10)
-goodness_all <- covarianceSelection::goodness_of_fit(dat_all, permutations = 250, trials = 250, prob = 1)
+goodness_all <- covarianceSelection::goodness_of_fit(dat_list, permutations = 250, trials = 250, prob = 1)
 
 save.image(file = paste0(save_filepath, "/step8_pfc35_goodness", filepath_suffix, ".RData"))
 
