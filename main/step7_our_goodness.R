d4a91a44a6d86beb6bf25323faa7c199583f8958
Kevin Lin <klin1234@gmail.com>
2019-11-06 19:06:00 -0500

    Trying new settings

--- main/step7_our_goodness.R
+++ main/step7_our_goodness.R
@@ -10,23 +10,23 @@ core_set <- selected_idx[covarianceSelection::clique_selection(g_sub, threshold 
 idx_our <- covarianceSelection::clique_selection(g_selected, threshold = gamma_threshold, target_idx = core_set)
 idx_our <- idx_our[[1]]
 
-goodness_our <- covarianceSelection::goodness_of_fit(dat_list[idx_our], permutations = 250, trials = 100, prob = 1,
-                                                           verbose = T)
-print(paste0("Finished our"))
+# goodness_our <- covarianceSelection::goodness_of_fit(dat_list[idx_our], permutations = 250, trials = 100, prob = 1,
+#                                                            verbose = T)
+# print(paste0("Finished our"))
 
 # dat_our <- do.call(rbind, dat_list[idx_our])
 # dat_our <- scale(dat_our, scale = F)
 # 
-# prob_vec <- c(1, 1-1e-5, 1-1e-5/2, 1-1e-4)
-# goodness_list <- vector("list", length = length(prob_vec))
-# 
-# for(i in 1:length(prob_vec)){
-#   set.seed(10)
-#   goodness_list[[i]] <- covarianceSelection::goodness_of_fit(dat_our, permutations = 250, trials = 100, prob = prob_vec[i],
-#                                                              verbose = T)
-#   print(paste0("Goodness ", i, " done"))
-#   save.image(file = paste0(save_filepath, "/step7_our_goodness", filepath_suffix, ".RData"))
-# }
+prob_vec <- c(1, 1-1e-5, 1-1e-4)
+goodness_list <- vector("list", length = length(prob_vec))
+
+for(i in 1:length(prob_vec)){
+  set.seed(10)
+  goodness_list[[i]] <- covarianceSelection::goodness_of_fit(dat_list[idx_our], permutations = 250, trials = 100, prob = prob_vec[i],
+                                                             verbose = T)
+  print(paste0("Goodness ", i, " done"))
+  save.image(file = paste0(save_filepath, "/step7_our_goodness", filepath_suffix, ".RData"))
+}
 
 save.image(file = paste0(save_filepath, "/step7_our_goodness", filepath_suffix, ".RData"))
 
